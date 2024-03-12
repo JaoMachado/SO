f3ddc2e302eeca78417123166c2546cb617c0793
JaoMachado <jp.dausi@hotmail.com>
2024-03-12 16:41:37 -0300

    .

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            long currentProcessId = ProcessHandle.current().pid();
+            System.out.println("PID do processo atual: " + currentProcessId);
+        } else {
+            Process process = new ProcessBuilder(args[0]).start();
+            ProcessHandle processHandle = process.toHandle();
+            long processId = processHandle.pid();
+
+            System.out.println("Nome do aplicativo: " + args[0]);
+            System.out.println("PID do processo: " + processId);
+        }
+    }
+}
